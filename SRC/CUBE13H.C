3f210221ed6241fcd40cf8e718afa6062a72d1e3
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2025-06-13 03:34:11 +0300

    refactor(SRC): remove APP, CUBE, FONTS, UI files and update MAKEFILE for streamlined structure

--- SRC/CUBE13H.C
+++ SRC/CUBE13H.C
@@ -1,11 +1,21 @@
-#include "CUBE.H"
+#include "VEC.H"
+#include "TRINGL.H"
+#include "TIMER.H"
+#include "KEYBRD.H"
 #include "RENDERER.H"
 
 #define WIDTH 320
 #define HEIGHT 200
 #define FOV_FACTOR 280
+#define V_COUNT 8
+#define FACE_COUNT (6 * 2)
 
-static const face faces[FACE_COUNT] = {
+static int isRunning;
+vec3 cubeRot;
+vec3 cameraPos;
+TriArray triangles;
+
+static const Face faces[FACE_COUNT] = {
     // front
     {1, 2, 3, 0x50},
     {1, 3, 4, 0x51},
@@ -37,30 +47,38 @@ static const vec3 vertices[V_COUNT] = {
     {-1, -1,  1},
 };
 
-Cube::Cube() {
-    cameraPos.z = 5;
+static void processInput(void) {
+    char key = kbHit();
+
+    if (key == ESC) {
+        isRunning = 0;
+    }
 }
 
-void Cube::update(void) {
+static void update(void) {
+    int i, j;
+    Face face;
+    vec3 cameraRay;
+    vec3 normal;
+    vec3 transformedVertex;
+    vec3 faceVertices[3];
+    vec3 transformedVertices[3];
+    vec2 projectedPoints[3];
+    Triangle projectedTriangle;
+
+    double dotNormalCamera;
+    
     sadd(&cubeRot, 0.02);
 
-    for (int i = 0; i < FACE_COUNT; i++) {
-        face face = faces[i];
-        
-        vec3 faceVertices[3] = {
-            {0, 0, 0},
-            {0, 0, 0},
-            {0, 0, 0}
-        };
+    for (i = 0; i < FACE_COUNT; i++) {
+        face = faces[i];
 
         faceVertices[0] = vertices[face.a - 1];
         faceVertices[1] = vertices[face.b - 1];
         faceVertices[2] = vertices[face.c - 1];
 
-        int j;
-        vec3 transformedVertices[3];
         for (j = 0; j < 3; j++) {
-            vec3 transformedVertex = faceVertices[j];
+            transformedVertex = faceVertices[j];
 
             transformedVertex = rotx(&transformedVertex, cubeRot.x);
             transformedVertex = roty(&transformedVertex, cubeRot.y);
@@ -74,15 +92,14 @@ void Cube::update(void) {
         }
         
         // Backface Culling
-        vec3 cameraRay = sub(&cameraPos, &transformedVertices[0]);
-        vec3 normal = computeNormal(transformedVertices);
+        cameraRay = sub(&cameraPos, &transformedVertices[0]);
+        normal = computeNormal(transformedVertices);
         
-        double dotNormalCamera = dot(&normal, &cameraRay);
+        dotNormalCamera = dot(&normal, &cameraRay);
     
         if (dotNormalCamera < 0) 
             continue;
 
-        vec2 projectedPoints[3];
         // Loop all three vertices to perform projection
         for (j = 0; j < 3; j++) {
             // Project the current vertex
@@ -93,32 +110,59 @@ void Cube::update(void) {
             projectedPoints[j].y += (HEIGHT >> 1);
         }
 
-        triangle projectedTriangle = {
-            {
-                {projectedPoints[0].x, projectedPoints[0].y},
-                {projectedPoints[1].x, projectedPoints[1].y},
-                {projectedPoints[2].x, projectedPoints[2].y},
-            },
-            face.color
-        };
+        projectedTriangle.points[0] = projectedPoints[0];
+        projectedTriangle.points[1] = projectedPoints[1];
+        projectedTriangle.points[2] = projectedPoints[2];
+        projectedTriangle.color = face.color;
         
-        triangles.push_back(projectedTriangle);
-    }
+		taPushback(&triangles, &projectedTriangle);
+    }    
 }
 
-void Cube::draw(void) {
-    for (int i = 0; i < triangles.size(); i++) {
-        triangle tri = triangles[i];
+static void render(void) {
+    int i;
+    Triangle* tri;
+
+  	rndClear(0x0);
+
+    for (i = 0; i < triangles.count; i++) {
+        tri = taAt(&triangles ,i);
 
         // Draw filled triangle
         rndDrawFilledTri(
-                tri.points[0].x, tri.points[0].y, // vertex A
-                tri.points[1].x, tri.points[1].y, // vertex B
-                tri.points[2].x, tri.points[2].y, // vertex C
-                tri.color
+                tri->points[0].x, tri->points[0].y, // vertex A
+                tri->points[1].x, tri->points[1].y, // vertex B
+                tri->points[2].x, tri->points[2].y, // vertex C
+                tri->color
         );  
     }
 
-    triangles.clear();
+   	taClear(&triangles);
+
+    rndUpdateBuffer();
 }
 
+
+
+void main(void) {
+	isRunning = 1;
+	cameraPos.z = 5;
+
+    rndInit();
+    kbInit();
+	_initTimer();
+    
+	taInit(&triangles, 12);
+    
+	while (isRunning) {
+        processInput();
+        update();
+        render();
+    }
+
+	taFree(&triangles);
+
+    rndExit();
+    kbExit();
+	_exitTimer();
+}
--- SRC/TRINGL.C
+++ SRC/TRINGL.C
@@ -1,4 +1,5 @@
 #include "TRINGL.H"
+#include <stdlib.h>
 
 vec3 computeNormal(vec3* vertices) {
     vec3 ab, ac, normal;
@@ -16,4 +17,51 @@ vec3 computeNormal(vec3* vertices) {
     normalize(&normal);
 
     return normal;
+}
+
+void taInit(TriArray* ta, int size) {
+    ta->count = 0;
+    ta->capacity = size;
+    ta->data = (Triangle*)malloc(size * sizeof(Triangle));
+}
+
+void taPushback(TriArray* ta, Triangle* tri) {
+    if (ta->count == ta->capacity) {
+        ta->capacity *= 2;
+        ta->data = (Triangle*)realloc(ta->data, ta->capacity * sizeof(Triangle));
+    }
+
+    ta->data[ta->count++] = *tri;
+}
+
+void taRemove(TriArray* ta, int index) {
+    int i;
+
+    if (index < 0 || index >= ta->count) {
+        return;
+    }
+    
+    for (i = index; i < ta->count - 1; i++) {
+        ta->data[i] = ta->data[i + 1];
+    }
+    
+    ta->count--;
+}
+
+Triangle* taAt(TriArray* ta, int index) {
+    if (index < 0 || index >= ta->count) {
+        return NULL;
+    }
+
+    return &ta->data[index];
+}
+
+void taClear(TriArray* ta) {
+    memset(ta->data, 0, ta->count * sizeof(Triangle));
+    ta->count = 0;
+}
+
+void taFree(TriArray* ta) {
+    free(ta->data);
+    ta->data = NULL;
 }
